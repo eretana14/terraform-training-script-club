f49032f4b7636d7be6d11a17fe81ec377f9ba106
eretana14 <eretana@akamai.com>
2025-09-25 10:07:06 -0600

    Data Sources Configuration

---
+++ main.tf
@@ -0,0 +1,28 @@
+#Akamai Group
+data "akamai_group" "single_group" {
+  group_name  = "Script Club"
+  contract_id = "ctr_1-5C13O2"
+}
+
+output "my_group_id" {
+  value = data.akamai_group.single_group.id
+}
+
+#Akamai AppSec Configuration
+data "akamai_appsec_configuration" "appsec_config" {
+  name = "akamaitechday"
+}
+
+output "my_appsec_config" {
+  value = data.akamai_appsec_configuration.appsec_config.host_names
+}
+
+#Akamai Property
+data "akamai_property" "my_property" {
+  name    = "eretana-scriptclub"
+  version = "2"
+}
+
+output "my_property" {
+  value = data.akamai_property.my_property.property_id
+}
