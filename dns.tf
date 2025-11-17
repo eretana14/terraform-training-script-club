1a5fe5bf24c539d4b9bad7cf96f3d0943ba0be00
eretana14 <eretana@akamai.com>
2025-11-17 10:20:11 -0600

    Meta Arguments

--- dns.tf
+++ dns.tf
@@ -1,8 +1,19 @@
+# resource "akamai_dns_record" "my_record_type_cname" {
+#      count = length(local.app_hostnames)
+#     zone       = "eretana-terraform.com"
+#     name       = local.app_hostnames[count.index]
+#     recordtype = "CNAME"
+#     ttl        = 300
+#     target     = [akamai_edge_hostname.edge_hostname.edge_hostname]
+# }
+
 resource "akamai_dns_record" "my_record_type_cname" {
-     count = length(local.app_hostnames)
-    zone       = "eretana-terraform.com"
-    name       = local.app_hostnames[count.index]
-    recordtype = "CNAME"
-    ttl        = 300
-    target     = [akamai_edge_hostname.edge_hostname.edge_hostname]
+    for_each = var.dns_records
+
+    zone       = each.value.zone
+    name       = each.value.name
+    recordtype = each.value.recordType
+    ttl        = each.value.ttl
+    target     = [each.value.target]
 }
+
--- variables.tf
+++ variables.tf
@@ -19,3 +19,73 @@ variable "apps" {
   type        = list(string)
   description = "For Interactions exercise"
 }
+
+variable "activate_latest_on_staging" {
+  type    = bool
+  default = true
+}
+
+variable "activate_latest_on_production" {
+  type    = bool
+  default = true
+}
+
+variable "contractid" {
+  type        = string
+  default     = ""
+  description = "Value unknown at the time of import. Please update."
+}
+
+variable "groupid" {
+  type        = string
+  default     = ""
+  description = "Value unknown at the time of import. Please update."
+}
+
+
+variable "dns_records" {
+  default = {
+    "api" = {
+      zone       = "eretana-terraform.com"
+      recordType = "CNAME"
+      ttl        = 300
+      target     = "eretana-terraform-scriptclub.test.edgesuite.net"
+      name       = "api.eretana-terraform.com"
+    },
+    "blog" = {
+      zone       = "eretana-terraform.com"
+      recordType = "CNAME"
+      ttl        = 300
+      target     = "eretana-terraform-scriptclub.test.edgesuite.net"
+      name       = "blog.eretana-terraform.com"
+    },
+    "cdn" = {
+      zone       = "eretana-terraform.com"
+      recordType = "CNAME"
+      ttl        = 300
+      target     = "eretana-terraform-scriptclub.test.edgesuite.net"
+      name       = "cdn.eretana-terraform.com"
+    },
+    "shop" = {
+      zone       = "eretana-terraform.com"
+      recordType = "CNAME"
+      ttl        = 300
+      target     = "eretana-terraform-scriptclub.test.edgesuite.net"
+      name       = "shop.eretana-terraform.com"
+    },
+    "www" = {
+      zone       = "eretana-terraform.com"
+      recordType = "CNAME"
+      ttl        = 300
+      target     = "eretana-terraform-scriptclub.test.edgesuite.net"
+      name       = "www.eretana-terraform.com"
+    },
+    "eretana" = {
+      zone       = "eretana-terraform.com"
+      recordType = "CNAME"
+      ttl        = 300
+      target     = "eretana-terraform-scriptclub.test.edgesuite.net"
+      name       = "eretana-terraform-scriptclub.eretana-terraform.com"
+    },
+  }
+}
